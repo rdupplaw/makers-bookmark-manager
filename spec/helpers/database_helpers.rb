bbdcb5c6486a3b5997a593d61f9e47ea6a8a71ec
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-19 12:50:47 +0100

    removed bookmarks helper and added persisted_data

--- spec/helpers/database_helpers.rb
+++ spec/helpers/database_helpers.rb
@@ -6,10 +6,7 @@ def wipe_table
   con.exec 'CREATE TABLE bookmarks (title VARCHAR(60), id SERIAL PRIMARY KEY, url VARCHAR(60));'
 end
 
-def add_test_bookmarks
-  con = PG.connect dbname: 'bookmark_manager_test'
-
-  con.exec "INSERT INTO bookmarks (title, url) VALUES ('search', 'http://www.google.com');"
-  con.exec "INSERT INTO bookmarks (title, url) VALUES ('cool bootcamp', 'http://www.makersacademy.com');"
-  con.exec "INSERT INTO bookmarks (title, url) VALUES ('tutorials', 'http://www.destroyallsoftware.com');"
+def persisted_data(id:)
+  PG.connect(dbname: 'bookmark_manager_test')
+    .query("SELECT * FROM bookmarks WHERE id = #{id};")
 end
