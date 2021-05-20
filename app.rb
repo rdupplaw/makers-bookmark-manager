57662ac87f1f79904b039e746b18dd74d3138220
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-20 17:21:13 +0100

    edit page shows existing data

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks/:id/edit' do
-    @bookmark_id = params['id']
+    @bookmark = Bookmark.find(id: params[:id])
     erb(:'bookmarks/edit')
   end
 
