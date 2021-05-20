d3473eacd8e6a5e0266d1a28b57fbb01bc50e838
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-20 20:02:11 +0100

    replaced PG calls with DatabaseConnection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'pg'
+require_relative './database_connection'
 
 # Imports bookmarks from database
 class Bookmark
@@ -13,69 +13,26 @@ class Bookmark
   end
 
   def self.all
-    database_name = if ENV['RACK_ENV'] == 'test'
-                      'bookmark_manager_test'
-                    else
-                      'bookmark_manager'
-                    end
-
-    connection = PG.connect(dbname: database_name)
-
-    response = connection.exec('SELECT * FROM bookmarks;')
-
+    response = DatabaseConnection.query('SELECT * FROM bookmarks;')
     response.map { |bookmark| Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) }
   end
 
   def self.create(title:, url:)
-    database_name = if ENV['RACK_ENV'] == 'test'
-                      'bookmark_manager_test'
-                    else
-                      'bookmark_manager'
-                    end
-
-    connection = PG.connect(dbname: database_name)
-
-    response = connection
-               .exec("INSERT INTO bookmarks (title, url) VALUES ('#{title}', '#{url}') RETURNING id, title, url;")
+    response = DatabaseConnection.query("INSERT INTO bookmarks (title, url) VALUES ('#{title}', '#{url}') RETURNING id, title, url;")
     Bookmark.new(id: response[0]['id'], title: response[0]['title'], url: response[0]['url'])
   end
 
   def self.delete(id:)
-    database_name = if ENV['RACK_ENV'] == 'test'
-                      'bookmark_manager_test'
-                    else
-                      'bookmark_manager'
-                    end
-
-    connection = PG.connect(dbname: database_name)
-
-    connection.exec("DELETE FROM bookmarks WHERE id=#{id};")
+    DatabaseConnection.query("DELETE FROM bookmarks WHERE id=#{id};")
   end
 
   def self.update(id:, title:, url:)
-    database_name = if ENV['RACK_ENV'] == 'test'
-                      'bookmark_manager_test'
-                    else
-                      'bookmark_manager'
-                    end
-
-    connection = PG.connect(dbname: database_name)
-
-    response = connection
-               .exec("UPDATE bookmarks SET title='#{title}', url='#{url}' WHERE id='#{id}' RETURNING id, title, url;")
+    response = DatabaseConnection.query("UPDATE bookmarks SET title='#{title}', url='#{url}' WHERE id='#{id}' RETURNING id, title, url;")
     Bookmark.new(id: response[0]['id'], title: response[0]['title'], url: response[0]['url'])
   end
 
   def self.find(id:)
-    database_name = if ENV['RACK_ENV'] == 'test'
-                      'bookmark_manager_test'
-                    else
-                      'bookmark_manager'
-                    end
-
-    connection = PG.connect(dbname: database_name)
-
-    response = connection.exec("SELECT * FROM bookmarks WHERE id='#{id}';")
+    response = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id='#{id}';")
     Bookmark.new(id: response[0]['id'], title: response[0]['title'], url: response[0]['url'])
   end
 end
