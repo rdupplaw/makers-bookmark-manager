75fafd17325d68658204f0e4e8f411ad90f1adb0
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-20 19:57:32 +0100

    added DatabaseConnection::query

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -4,11 +4,15 @@ require 'pg'
 
 # Handles persistent connection to database
 class DatabaseConnection
+  class << self
+    attr_reader :connection
+  end
+
   def self.setup(dbname)
     @connection = PG.connect(dbname: dbname)
   end
 
-  class << self
-    attr_reader :connection
+  def self.query(sql_string)
+    connection.exec(sql_string)
   end
 end
