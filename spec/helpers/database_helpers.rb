b08a708fddcf4502d6552a7979952775290ff294
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-21 12:40:23 +0100

    wipe_table recreates comments table

--- spec/helpers/database_helpers.rb
+++ spec/helpers/database_helpers.rb
@@ -2,8 +2,9 @@
 
 def wipe_table
   con = PG.connect dbname: 'bookmark_manager_test'
-  con.exec 'DROP TABLE IF EXISTS bookmarks'
+  con.exec 'DROP TABLE IF EXISTS bookmarks, comments'
   con.exec 'CREATE TABLE bookmarks (title VARCHAR(60), id SERIAL PRIMARY KEY, url VARCHAR(60));'
+  con.exec 'CREATE TABLE comments (id SERIAL PRIMARY KEY, text VARCHAR(240), bookmark_id INTEGER REFERENCES bookmarks (id));'
 end
 
 def persisted_data(id:)
